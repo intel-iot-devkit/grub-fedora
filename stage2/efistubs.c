86173c2550cadc18eeb883965cc5846222dd6913
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-10 02:10:40 +0800

    Delete stage2 directory

--- stage2/efistubs.c
+++
@@ -1,7 +0,0 @@
-
-#include "shared.h"
-#include "efistubs.h"
-
-#if defined(PLATFORM_EFI)
-int network_ready = 0;
-#endif /* defined(PLATFORM_EFI) */
--- stage2/efistubs.h
+++
@@ -1,8 +0,0 @@
-#ifndef EFISTUBS_H
-#define EFISTUBS_H 1
-
-#if defined(PLATFORM_EFI)
-extern int network_ready;
-#endif /* defined(PLATFORM_EFI) */
-
-#endif /* EFISTUBS_H */
